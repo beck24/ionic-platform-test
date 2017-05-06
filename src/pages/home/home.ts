5a82a9208e0f0af4c6c673aa7b5e896ebe58409b
Matt Beckett <beck24@gmail.com>
2017-05-06 13:13:24 -0700

    initial commit

---
+++ src/pages/home/home.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { NavController, Platform } from 'ionic-angular';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+  platformList: string = '';
+  isApp: boolean = false;
+
+  constructor(
+    public navCtrl: NavController,
+    public platform: Platform
+    ) {
+
+      let platforms = this.platform.platforms();
+
+      this.platformList = platforms.join(', ');
+
+      if (this.platform.is('core') || this.platform.is('mobileweb')) {
+        this.isApp = false;
+      }
+  }
+
+
+}
